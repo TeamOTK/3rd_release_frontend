92bf278c838a62ff1b50b32f161fc2e6ff44c573
hsh <hsh200315@naver.com>
2024-03-08 20:02:51 +0900

    모달 글 수정

--- src/components/EventModal.jsx
+++ src/components/EventModal.jsx
@@ -9,7 +9,8 @@ const EventModal = (props) =>{
 				</Modal.Title>
 			</Modal.Header>
 			<Modal.Body>
-         <div>설문에 참여해주신분 중 추첨을 통해 경품을 지급할 예정입니다. 차차 사용 이후 솔직한 후기를 설문에 남겨주세요!</div>
+         <div>설문에 참여해주신분 중 추첨을 통해 경품을 지급할 예정입니다.</div>
+				 <div>사용 후기를 설문에 남겨주세요!</div>
 				 <div className="d-flex justify-content-center">
 				 <Button size="lg" onClick={()=>window.open("https://t.co/OXqqe4K3eX")} className="SurveyBtn">설문</Button>
 				 </div>
